afcc952f129a4f59aceb888bb225ceac3ba80953
BelcanD <belcand@mail.ru>
2024-12-17 16:49:15 +0300

    Adjusted positioning of About Me, Name, and Title in PDF generation

--- app.py
+++ app.py
@@ -469,13 +469,13 @@ def create_pdf(data, photo=None):
     c.drawString(title_x_position, title_y_position, data['title'])  # Draw title
 
     # Adjust "About Me" section position
-    y_position += 30  # Move "About Me" section down slightly
+    y_position += 10  # Increase this value to move "About Me" section down
 
     # About me section
     c.setFont("Helvetica-Bold", 16)
     c.setFillColorRGB(1, 1, 1)
     c.drawString(20, y_position, "About me")
-    y_position -= 25
+    y_position -= 10
     
     # About text with wrapping
     y_position = wrap_text(data['about'], sidebar_width - 40, c, 20, y_position, "Helvetica", 10)
